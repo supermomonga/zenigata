538edcdfa91ce668adb78118a4e7e81968d2dddc
supermomonga <hi@supermomonga.com>
2014-09-10 00:53:18 +0900

    pages controller

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,4 @@
 RSpec.configure do |config|
-  # rspec-expectations config goes here. You can use an alternate
-  # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you prefer.
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
     expectations.syntax = :expect
