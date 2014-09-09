9b5dcd7bc09477cf00ca5eb1bce76f0b72781470
supermomonga <hi@supermomonga.com>
2014-09-10 06:33:52 +0900

    bootstrap helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def icon name, space = ' '
+    raw %`<span class="glyphicon glyphicon-#{name}"></span>#{space}`
+  end
+
+  def link_with_icon_to title, icon_name, *args
+    link_to raw("#{icon icon_name}#{title}"), *args
+  end
+
+  def caret lspace = ' ', rspace = ''
+    raw %`#{lspace}<span class="caret"></span>#{rspace}`
+  end
+
 end
