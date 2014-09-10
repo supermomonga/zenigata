fa9922b9b263e2e2ef4b222acabda0019476a65b
supermomonga <hi@supermomonga.com>
2014-09-11 03:22:18 +0900

    automatic locale detect and japanese locale file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :detect_locale
+
+  private
+  def detect_locale
+    I18n.locale = request.headers['Accept-Language'].scan(/^[a-z]{2}/).first
+  end
 end
